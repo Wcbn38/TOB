d4f5cc6e6deea5a66d571a631221c43c092d679b
Wcbn38 <37038441+Wcbn38@users.noreply.github.com>
2020-12-07 20:40:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ bot.on("ready", channels => {
 })
 
 bot.on("message", message => {
-    if ( message.member.id !== bot.client.user.id )
+    if ( message.member.id !== bot.client.id )
     {
         var s_message = new String();
         var s_newMessage = new String();
