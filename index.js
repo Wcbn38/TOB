a01c68da22e6c9418fc761360d1fcb8939cc0f98
Wcbn38 <37038441+Wcbn38@users.noreply.github.com>
2020-12-07 19:24:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+//recuperer l'ID role discord == \@rolename
 
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const adminChannel = process.env.adminChannel
+
+var http = require("http");
+setInterval(function () {
+    http.get("http://wcbn-s-bot.herokuapp.com/");
+}, 900000)
+
+bot.on("ready", channels => {
+  bot.channels.get(adminChannel).send(`Bot started.`)
+})
+
+bot.on("message", message => {
+
+})
+
+bot.on("channelCreate", channel => {
+
+})
+
+
+bot.login(process.env.TOKEN);
